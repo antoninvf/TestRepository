e68759344bf66e0dd06124bc24c415e82bb7e510
Antonin Vondrovic <antonin.vondrovic@educanet.cz>
2020-10-19 09:34:17 +0200

    added useless comments to Main.java

--- test/src/Main.java
+++ test/src/Main.java
@@ -5,10 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //This makes it say that
         System.out.println("Say something and I will repeat it.");
 
+        //This allows the user to write something
         String input = sc.nextLine();
 
+        //This outputs what the user has said
         System.out.println(input);
 
     }
