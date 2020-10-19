1a374a3e3098be13fc32c28baa76e8059136e4c6
Antonin Vondrovic <you@example.com>
2020-10-19 09:25:34 +0200

    added a simple java project

---
+++ test/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Say something and I will repeat it.");
+
+        String input = sc.nextLine();
+
+        System.out.println(input);
+
+    }
+
+}
